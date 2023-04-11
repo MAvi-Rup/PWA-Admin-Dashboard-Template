10b99f0edfb19589b33d55a9c5ce67ddf9cf284f
MAvi-Rup <avirupmondal19@gmail.comm>
2023-04-11 14:05:22 +0600

    Form Validation Done

--- src/components/FieldTechnitian/FarmerRegistrationForm.jsx
+++ src/components/FieldTechnitian/FarmerRegistrationForm.jsx
@@ -1,7 +1,9 @@
 import React from "react";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Formik, Form, Field, ErrorMessage, setFieldValue } from "formik";
 import * as Yup from "yup";
 import './FarmerRegistrationForm.css'
+import { useState } from "react";
+import { UilLocationArrow } from "@iconscout/react-unicons";
 
 const initialValues = {
     name: "",
@@ -22,30 +24,42 @@ const initialValues = {
 };
 
 const validationSchema = Yup.object().shape({
-    name: Yup.string().required("Name is required"),
-    fatherName: Yup.string().required("Father name is required"),
+    name: Yup.string()
+        .required("Name is required")
+        .min(3, "Name must be at least 3 characters"),
+    fatherName: Yup.string()
+        .required("Father name is required")
+        .min(3, "Father name must be at least 3 characters"),
     village: Yup.string().required("Village is required"),
     upjilla: Yup.string().required("Upjilla is required"),
     gilla: Yup.string().required("Gilla is required"),
-    nid: Yup.string().required("NID is required"),
+    nid: Yup.string()
+        .required("NID is required")
+        .matches(/^[0-9]+$/, "NID must be a number")
+        .min(10, "NID must be at least 10 digits"),
     mobileNo: Yup.string()
         .required("Mobile no is required")
-        .matches(/^[0-9]+$/, "Must be only digits")
-        .min(10, "Must be exactly 10 digits")
-        .max(10, "Must be exactly 10 digits"),
+        .matches(/^[0-9]+$/, "Mobile no must be a number")
+        .min(10, "Mobile no must be exactly 10 digits")
+        .max(10, "Mobile no must be exactly 10 digits"),
     location: Yup.string().required("Location is required"),
     plantation: Yup.number()
         .required("Plantation is required")
-        .positive("Must be a positive number"),
+        .positive("Plantation must be a positive number"),
     target: Yup.number()
         .required("Target is required")
-        .positive("Must be a positive number"),
-    accountNo: Yup.string().required("Account no is required"),
+        .positive("Target must be a positive number"),
+    accountNo: Yup.string()
+        .required("Account no is required")
+        .matches(/^[0-9]+$/, "Account no must be a number"),
     bankName: Yup.string().required("Bank name is required"),
     branch: Yup.string().required("Branch is required"),
-    routingNo: Yup.string().required("Routing no is required"),
+    routingNo: Yup.string()
+        .required("Routing no is required")
+        .matches(/^[0-9]+$/, "Routing no must be a number"),
     farmerPic: Yup.mixed().required("Farmer picture is required"),
 });
+
 
 const onSubmit = (values) => {
     console.log(values);
@@ -53,6 +67,33 @@ const onSubmit = (values) => {
 };
 
 const FarmerRegistrationForm = () => {
+    const [locationError, setLocationError] = useState("");
+    const [isFetchingLocation, setIsFetchingLocation] = useState(false);
+    const [locationFieldValue, setLocationFieldValue] = useState("");
+
+
+    const getLocation = (setFieldValue) => {
+        setIsFetchingLocation(true);
+
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    const { latitude, longitude } = position.coords;
+                    const location = `${latitude},${longitude}`;
+                    setLocationFieldValue(location);
+                    setFieldValue("location", location);
+                    setIsFetchingLocation(false);
+                },
+                (error) => {
+                    setIsFetchingLocation(false);
+                    setLocationError(error.message);
+                }
+            );
+        } else {
+            setLocationError("Geolocation is not supported by this browser.");
+            setIsFetchingLocation(false);
+        }
+    };
     return (
         <div className="form-container fadeInUp">
             <h1>Farmer Registration Form</h1>
@@ -66,104 +107,165 @@ const FarmerRegistrationForm = () => {
                         <div className="form-group">
                             <label htmlFor="name">Name</label>
                             <Field type="text" name="name" />
-                            <ErrorMessage name="name" className="error-message" />
+                            <div className="error-message" >
+                                <ErrorMessage name="name" />
+                            </div>
+
                         </div>
 
                         <div className="form-group">
                             <label htmlFor="fatherName">Father Name</label>
                             <Field type="text" name="fatherName" />
-                            <ErrorMessage name="fatherName" className="error-message" />
+                            <div className="error-message">
+                                <ErrorMessage name="fatherName" />
+                            </div>
                         </div>
 
                         <div className="form-group">
                             <label htmlFor="village">Village</label>
                             <Field type="text" name="village" />
-                            <ErrorMessage name="village" className="error-message" />
+                            <div className="error-message">
+                                <ErrorMessage name="village" className="error-message" />
+                            </div>
+
                         </div>
 
                         <div className="form-group">
                             <label htmlFor="upjilla">Thana/Upjilla</label>
                             <Field type="text" name="upjilla" />
-                            <ErrorMessage name="upjilla" className="error-message" />
+                            <div className="error-message" >
+                                <ErrorMessage name="upjilla" />
+                            </div>
+
                         </div>
 
                         <div className="form-group">
                             <label htmlFor="gilla">Gilla</label>
                             <Field type="text" name="gilla" />
-                            <ErrorMessage name="gilla" className="error-message" />
+                            <div className="error-message">
+                                <ErrorMessage name="gilla" />
+
+                            </div>
+
                         </div>
                         <div className="form-group">
-                        <label htmlFor="nid">NID</label>
-                        <Field type="text" name="nid" />
-                        <ErrorMessage name="nid" className="error-message" />
-                    </div>
+                            <label htmlFor="nid">NID</label>
+                            <Field type="text" name="nid" />
+                            <div className="error-message">
+                                <ErrorMessage name="nid" />
 
-                    <div className="form-group">
-                        <label htmlFor="mobileNo">Mobile No</label>
-                        <Field type="text" name="mobileNo" />
-                        <ErrorMessage name="mobileNo" className="error-message" />
-                    </div>
+                            </div>
 
-                    <div className="form-group">
-                        <label htmlFor="location">Location</label>
-                        <Field type="text" name="location" />
-                        <ErrorMessage name="location" className="error-message" />
-                    </div>
+                        </div>
 
-                    <div className="form-group">
-                        <label htmlFor="plantation">Plantation</label>
-                        <Field type="number" name="plantation" />
-                        <ErrorMessage name="plantation" className="error-message" />
-                    </div>
+                        <div className="form-group">
+                            <label htmlFor="mobileNo">Mobile No</label>
+                            <Field type="text" name="mobileNo" />
+                            <div className="error-message">
+                                <ErrorMessage name="mobileNo" />
 
-                    <div className="form-group">
-                        <label htmlFor="target">Target</label>
-                        <Field type="number" name="target" />
-                        <ErrorMessage name="target" className="error-message" />
-                    </div>
+                            </div>
 
-                    <div className="form-group">
-                        <label htmlFor="accountNo">Account No</label>
-                        <Field type="text" name="accountNo" />
-                        <ErrorMessage name="accountNo" className="error-message" />
-                    </div>
+                        </div>
 
-                    <div className="form-group">
-                        <label htmlFor="bankName">Bank Name</label>
-                        <Field type="text" name="bankName" />
-                        <ErrorMessage name="bankName" className="error-message" />
-                    </div>
+                        <div className="form-group">
+                            <label htmlFor="location">Location</label>
+                            <Field type="text" name="location" value={locationFieldValue} />
+                            {locationError && (
+                                <div className="error-message">{locationError}</div>
+                            )}
+                            <button
+                                type="button"
+                                onClick={() => getLocation(setFieldValue)}
+                                disabled={isFetchingLocation}
+                                className="location-btn"
+                            >
+                                {isFetchingLocation ? "Fetching location..." : <UilLocationArrow />}
+                            </button>
+                        </div>
 
-                    <div className="form-group">
-                        <label htmlFor="branch">Branch</label>
-                        <Field type="text" name="branch" />
-                        <ErrorMessage name="branch" className="error-message" style={{color:'red'}}/>
-                    </div>
+                        <div className="form-group">
+                            <label htmlFor="plantation">Plantation</label>
+                            <Field type="number" name="plantation" />
+                            <div className="error-message" >
+                                <ErrorMessage name="plantation" />
 
-                    <div className="form-group">
-                        <label htmlFor="routingNo">Routing No</label>
-                        <Field type="text" name="routingNo" />
-                        <ErrorMessage name="routingNo" />
-                    </div>
+                            </div>
 
-                    <div className="form-group">
-                        <label htmlFor="farmerPic">Farmer Picture</label>
-                        <Field
-                            type="file"
-                            name="farmerPic"
-                            onChange={(event) => {
-                                setFieldValue("farmerPic", event.currentTarget.files[0]);
-                            }}
-                        />
-                        <ErrorMessage name="farmerPic" className="error-message" />
-                    </div>
+                        </div>
 
-                    <button type="submit">Submit</button>
-                </Form>
-            )}
-        </Formik>
-    </div>
-);
+                        <div className="form-group">
+                            <label htmlFor="target">Target</label>
+                            <Field type="number" name="target" />
+                            <div className="error-message" >
+                                <ErrorMessage name="target" />
+
+                            </div>
+
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="accountNo">Account No</label>
+                            <Field type="text" name="accountNo" />
+                            <div className="error-message" >
+                                <ErrorMessage name="accountNo" />
+
+                            </div>
+
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="bankName">Bank Name</label>
+                            <Field type="text" name="bankName" />
+                            <div className="error-message">
+                                <ErrorMessage name="bankName" />
+
+                            </div>
+
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="branch">Branch</label>
+                            <Field type="text" name="branch" />
+                            <div className="error-message">
+                                <ErrorMessage name="branch" />
+
+                            </div>
+
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="routingNo">Routing No</label>
+                            <Field type="text" name="routingNo" />
+                            <div className="error-message">
+                                <ErrorMessage name="routingNo" />
+
+                            </div>
+
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="farmerPic">Farmer Picture</label>
+                            <Field
+                                type="file"
+                                name="farmerPic"
+                                onChange={(event) => {
+                                    setFieldValue("farmerPic", event.currentTarget.files[0]);
+                                }}
+                            />
+                            <div className="error-message" >
+                                <ErrorMessage name="farmerPic" />
+
+                            </div>
+
+                        </div>
+
+                        <button type="submit">Submit</button>
+                    </Form>
+                )}
+            </Formik>
+        </div>
+    );
 };
 
 export default FarmerRegistrationForm;
